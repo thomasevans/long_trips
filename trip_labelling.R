6a850d40ceee2e419574d40ef7183cf709681162
thomasevans <thomas.jude.evans@gmail.com>
2017-01-26 13:03:09 +0100

    Label foraging trips

---
+++ trip_labelling.R
@@ -0,0 +1,28 @@
+# Trip definition and numbering
+
+
+# Require packages ------
+
+
+
+# Load in data ------
+
+
+# GPS data with calculated variables
+
+
+# fix data structure (if needed)
+
+
+
+# Point 'on trip', if over distance threshold -------
+
+
+
+# Go through points and number trips ------
+
+
+
+# Put info together in table for export ----
+
+# Include, on_trip, trip_id, device_info_serial, and date_time
