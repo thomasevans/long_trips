0167c35cd61860ca267e74b7ed43dbb9a6b96e8d
thomasevans <thomas.jude.evans@gmail.com>
2017-01-26 14:46:20 +0100

    Summarise information about trips

---
+++ trip_info_summary.R
@@ -0,0 +1,40 @@
+# Trip info summary
+
+
+# Make a summary table of 'trips'
+# Include, e.g. max dist, duration, max time gap...
+
+
+# Required R package -----
+
+
+
+# Load in data ------
+
+
+
+# Fix data structure
+
+
+
+# Make unique table to trips -----
+# E.g. 'unique ...'
+# To get device_info_serial, trip_id, ring_number...
+
+
+# Use dplyr + plyr?? to summarise foraging trips -----
+
+
+
+
+# Combine trip paramater summary table with trip ID summary stuff -----
+
+
+# Check the format
+
+
+# Output this to database ------
+
+
+
+
