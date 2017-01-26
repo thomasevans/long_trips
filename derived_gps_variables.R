502aab557c57675bb20ccdc9788f74bac8e5504b
thomasevans <thomas.jude.evans@gmail.com>
2017-01-26 08:31:02 +0100

    Plan for code

---
+++ derived_gps_variables.R
@@ -0,0 +1,41 @@
+# Derived variables from GPS
+
+
+# Required packages -------
+
+
+
+# Load in data (connect to DB) --------
+
+# Load in GPS point data
+
+
+# Load in bird/ GPS deployment data
+# (includes nest location used for distance from colony)
+
+
+
+# Calculate distance from colony -------
+
+
+
+# Calculate inter-point distance ------
+# (Distance moved between fixes)
+
+
+
+# Calculate time between fixes (time interval) -------
+
+
+
+# Combine into dataframe for export ------
+
+
+
+# Export dataframe to DB -------
+
+
+
+
+
+
